4ea4f5be7de31c9e750cda053e34672d4554f3a3
Foridul islam <foridulislamdise880@gmail.com>
2026-06-17 19:01:19 +0600

    database connect and basic express cors dotenv setup

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const connectDB = require('./db/db');
+const dotenv =require('dotenv');
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB 
+connectDB();
+
+
+
+// Routes
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/products', require('./routes/products'));
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the KFTR API');
+});
+
+// error handling middleware
+app.use(( req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+module.exports = app;
